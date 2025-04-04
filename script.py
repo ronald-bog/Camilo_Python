838013abd0b30101af6248984cc1542852330e6f
Ronald Espitia <ronald79@gmail.com>
2025-04-04 17:08:43 -0500

    Clase

---
+++ script.py
@@ -0,0 +1,11 @@
+
+def saludo():
+    print("Hola Mundo")
+
+def sumar(b):
+    return 10 + b
+
+resultado = sumar(15)
+
+print(resultado)
+
---
+++ script2.py
@@ -0,0 +1,24 @@
+# listas
+
+placas = ["AAA111", "BBB222", "CCC333", "DDD444"]
+
+print(placas)
+
+placas.append("HIB132")
+
+print(placas)
+
+placas.pop(2)
+
+print(placas)
+
+placas.pop(2)
+
+print(placas)
+
+
+placas2 = ["AAA111", "BBB222", "CCC333", "DDD444"]
+
+placas2.insert(1,"GSD874")
+
+print(placas2)
